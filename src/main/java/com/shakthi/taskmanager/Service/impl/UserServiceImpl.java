b7e916d0c5c7a56a43f1f66eb82edc617e8d139a
Shakthi Pranav <shakthipranav05@gmail.com>
2025-12-15 18:49:45 +0530

    Added password hashing

--- src/main/java/com/shakthi/taskmanager/Service/impl/UserServiceImpl.java
+++ src/main/java/com/shakthi/taskmanager/Service/impl/UserServiceImpl.java
@@ -3,28 +3,29 @@ package com.shakthi.taskmanager.Service.impl;
 import com.shakthi.taskmanager.Model.User;
 import com.shakthi.taskmanager.Repository.UserRepository;
 import com.shakthi.taskmanager.Service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-
     private final UserRepository userRepository;
-
-    public UserServiceImpl(UserRepository userRepository) {
+    private final PasswordEncoder passwordEncoder;
+    public UserServiceImpl(UserRepository userRepository,
+                           PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User registerUser(User user) {
-
         if (userRepository.findByUsername(user.getUsername()).isPresent()) {
             throw new RuntimeException("Username already exists");
         }
-
         if (userRepository.findByEmail(user.getEmail()).isPresent()) {
             throw new RuntimeException("Email already exists");
         }
-
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
+
 }
